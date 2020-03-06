6208b0f80028b8ba0df9349f85bcbba0bd95c84d
Brooke Tingley <btingley@ycp.edu>
2020-03-05 22:33:33 -0500

    Adding Game, Parser, and Word

---
+++ Parser.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Parser
+{
+  ArrayList verbBank;
+  ArrayList nounBank;
+  ArrayList directionBank;
+  ArrayList actionBank;
+  Map synonymToWord;
+
+  public Parser(ArrayList verbBank, ArrayList nounBank, ArrayList directionBank, ArrayList actionBank, Map synonymToWord) {
+	  this.verbBank = verbBank;
+    this.nounBank = nounBank;
+    this.directionBank = directionBank;
+    this.actionBank = actionBank;
+    this.synonymToWord = synonymToWord;
+  	}
+
+  public void getInput() {
+    return;
+  }
+
+  public void checkInput() {
+    return;
+  }
+
+  public void checkSynonyms() {
+    return;
+  }
+
+  public void performAction() {
+    return;
+  }
+
+}
