ba0c3ddf24bf032e095c0a0bba1f6d81540b1822
Brooke Tingley <btingley@ycp.edu>
2020-03-12 21:50:22 -0400

    Added methods to room/item/inventory

--- Inventory.java
+++ Inventory.java
@@ -2,11 +2,49 @@ import java.util.*;
 
 public class Inventory {
 	int maxSize;
-  ArrayList items;
+  ArrayList items = new ArrayList();
 
 	public Inventory(int maxSize) {
 		this.maxSize = maxSize;
-    this.items = new ArrayList();
+    this.items = items;
+	}
+
+	public int getSize() {
+		return maxSize;
+	}
+
+	public void setMaxSize(int maxSize) {
+		this.maxSize = maxSize;
+		return;
+	}
+
+	public boolean checkSize() {
+		//TODO
+		return true;
+	}
+
+	public ArrayList getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList items) {
+		this.items = items;
+		return;
+	}
+
+	public void addItems(ArrayList items) {
+		//TODO
+		return;
+	}
+
+	public void dropItem(Item item) {
+		//TODO
+		return;
+	}
+
+	public void removeItems(Item items) {
+		//TODO
+		return;
 	}
 
 	public boolean checkValidity() {
