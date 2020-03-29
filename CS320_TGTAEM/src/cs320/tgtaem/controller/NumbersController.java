66d937b57939c500e50810a608df147a0259a48d
regreaves <rgreaves@ycp.edu>
2020-03-28 21:24:28 -0400

    Adding skeleton web.

---
+++ CS320_TGTAEM/src/cs320/tgtaem/controller/NumbersController.java
@@ -0,0 +1,27 @@
+package cs320.tgtaem.controller;
+
+import cs320.tgtaem.model.Numbers;
+
+/**
+ * Controller for numbers.
+ */
+public class NumbersController {
+	private Numbers model;
+
+	/**
+	 * Set the model.
+	 * 
+	 * @param model the model to set
+	 */
+	public void setModel(Numbers model) {
+		this.model = model;
+	}
+
+	public void add(double first, double second, double third) {
+		model.setResult(first + second + third);
+	}
+	
+	public void multiply(double first, double second, double third) {
+		model.setResult(first * second * third);
+	}
+}
---
+++ CS320_TGTAEM/war/_view/index.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Index view</title>
+	</head>
+
+	<body>		
+		<form action="${pageContext.servletContext.contextPath}/addNumbers" method="get">
+			<input type="Submit" name="submit" value="Add Numbers!">
+		</form>
+	</body>
+</html>
