8fb0372309d857a347ce0cebd99c3cba82b72cea
Brooke Tingley <btingley@ycp.edu>
2020-03-30 21:12:17 -0400

    JUnit tests for Inventory and Items

---
+++ ta_eclipse/junit/objectstests/InventoryTests.java
@@ -0,0 +1,71 @@
+package objectstests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import objects.Inventory;
+import objects.Item;
+
+public class InventoryTests {
+	private Inventory inv1;
+	private Inventory inv2;
+	private Inventory inv3;
+	
+	private ArrayList<Item> items1 = new ArrayList<>();
+	private ArrayList<Item> items2 = new ArrayList<>();
+	private ArrayList<Item> items3 = new ArrayList<>();
+	
+	private Item sword;
+	private Item axe;
+	private Item flower;
+	
+	@Before
+	public void setUp() {
+		sword = new Item("1", "sword", "A steel sword.", "A sword that is light.", true, false);
+		axe = new Item("2", "axe", "An iron axe.", "An iron axe that is heavy.", true, false);
+		flower = new Item("3", "flower", "A red rose.", "The thorns are sharp.", true, false);
+		
+		items1.add(sword);
+		items1.add(axe);
+		
+		items2.add(flower);
+		
+		inv1 = new Inventory(10, "inventory one", items1);
+		inv2 = new Inventory(1, "inventory two", items2);
+		inv3 = new Inventory(5, "inventory three", items3);
+	}
+	
+	@Test
+	public void testGetSize() throws Exception {
+		assertEquals(10, inv1.getSize());
+		assertEquals(1, inv2.getSize());
+		assertEquals(5, inv3.getSize());
+	}
+	
+	@Test
+	public void testCheckSize() throws Exception {
+		assertTrue(inv1.checkSize());
+		assertFalse(inv2.checkSize());
+		assertTrue(inv3.checkSize());
+	}
+	
+	@Test
+	public void testGetCurrentSize() throws Exception {
+		assertEquals(8, inv1.getCurrentSize());
+		assertEquals(0, inv2.getCurrentSize());
+		assertEquals(5, inv3.getCurrentSize());
+	}
+	
+	@Test
+	public void testGetNumberItems() throws Exception {
+		assertEquals(2, inv1.getNumberItems());
+		assertEquals(1, inv2.getNumberItems());
+		assertEquals(0, inv3.getNumberItems());
+	}
+}
---
+++ ta_eclipse/junit/objectstests/ItemTests.java
@@ -0,0 +1,66 @@
+package objectstests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import objects.Item;
+
+public class ItemTests {
+	
+	private Item sword;
+	private Item axe;
+	private Item flower;
+	
+	@Before
+	public void setUp() {
+		sword = new Item("1", "sword", "A steel sword.", "A sword that is light.", true, false);
+		axe = new Item("2", "axe", "An iron axe.", "An iron axe that is heavy.", true, false);
+		flower = new Item("3", "flower", "A red rose.", "The thorns are sharp.", true, false);
+	}
+	
+	@Test
+	public void testGetID() throws Exception {
+		assertEquals("1", sword.getID());
+		assertEquals("2", axe.getID());
+		assertEquals("3", flower.getID());
+	}
+	
+	@Test
+	public void testGetName() throws Exception {
+		assertEquals("sword", sword.getName());
+		assertEquals("axe", axe.getName());
+		assertEquals("flower", flower.getName());
+	}
+	
+	@Test
+	public void testGetInitDscrpt() throws Exception {
+		assertEquals("A steel sword.", sword.getInitDscrpt());
+		assertEquals("An iron axe.", axe.getInitDscrpt());
+		assertEquals("A red rose.", flower.getInitDscrpt());
+	}
+	
+	@Test
+	public void testGetInventDscrpt() throws Exception {
+		assertEquals("A sword that is light.", sword.getInventDscrpt());
+		assertEquals("An iron axe that is heavy.", axe.getInventDscrpt());
+		assertEquals("The thorns are sharp.", flower.getInventDscrpt());
+	}
+	
+	@Test
+	public void testCanTake() throws Exception {
+		assertTrue(sword.canTake());
+		assertTrue(axe.canTake());
+		assertTrue(flower.canTake());
+	}
+	
+	@Test
+	public void testIsHidden() throws Exception {
+		assertFalse(sword.isHidden());
+		assertFalse(axe.isHidden());
+		assertFalse(flower.isHidden());
+	}
+}
