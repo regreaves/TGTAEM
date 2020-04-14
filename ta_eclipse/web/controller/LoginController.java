0147f2f703141cd118e53750fc4002e62acdff48
regreaves <rgreaves@ycp.edu>
2020-04-13 20:50:56 -0400

    Updated LoginController.java.

--- ta_eclipse/web/controller/LoginController.java
+++ ta_eclipse/web/controller/LoginController.java
@@ -1,8 +1,19 @@
 package controller;
 
-/**
- * Controller for Login.
- */
+import state.Library;
+
 public class LoginController {
+	private Library model = null;
 	
+	public LoginController(Library model) {
+		this.model = model;
+	}
+	
+	public boolean checkUserName(String name) {
+		return model.validateUserName(name);
+	}
+	
+	public boolean validateCredentials(String name, String pw) {
+		return model.validatePW(name, pw);
+	}
 }
